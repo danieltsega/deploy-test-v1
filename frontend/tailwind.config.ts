80a9c6b40572b41a843d041ab9654106d4499b4c
danieltsega <danieltsega6658@gmail.com>
2025-08-26 16:19:58 -0400

    feat: add next js app

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+// frontend/tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // Example custom color
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
