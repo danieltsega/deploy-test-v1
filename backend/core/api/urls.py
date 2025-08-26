0ee8138b6b700a7bb9448cb65d62de68885c0caa
danieltsega <danieltsega6658@gmail.com>
2025-08-26 16:48:47 -0400

    update: add the new views as url

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,7 +1,9 @@
 # core/api/urls.py
 from django.urls import path
-from .views import HelloView
+from .views import TodoListCreateView, TodoDetailView
 
 urlpatterns = [
-    path('hello/', HelloView.as_view(), name='hello'),
+    #path('hello/', HelloView.as_view(), name='hello'),
+    path('todos/', TodoListCreateView.as_view(), name='todo-list-create'),
+    path('todos/<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
 ]
